0892a3d2cc75a74002f6a54dffeeab224a8f308e
Eric Wajnrajch <ericwaje03@gmail.com>
2022-04-21 17:02:35 -0300

    iniciando proyecto de useReducer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+    return (
+        <>
+            <h1>3 Ejemplos de useReducer</h1>
+            <Counter />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,69 @@
+import React, { useReducer } from 'react';
+
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case 'suma':
+            return state + 1;
+        case 'resta':
+            return state - 1;
+
+        default:
+            return state;
+    }
+};
+
+const Counter = () => {
+    const [counter, dispatch] = useReducer(counterReducer, 0);
+
+    //se puede llamar al dispatch desde una función
+    const resta = () => {
+        dispatch({
+            type: 'resta',
+        });
+    };
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'orange',
+                minHeight: '70vh',
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <h2>useReducer en un contador 🤔</h2>
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                {/* se puede llamar al dispatch directo desde el onClick */}
+                <button
+                    onClick={() =>
+                        dispatch({
+                            type: 'suma',
+                        })
+                    }
+                >
+                    +
+                </button>
+                <p
+                    style={{
+                        width: '50px',
+                        textAlign: 'center',
+                        fontSize: '20px',
+                    }}
+                >
+                    {counter}
+                </p>
+                <button onClick={resta}>-</button>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
